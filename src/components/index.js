00abb051e9dad7fe6acd558dfe7a6a7491588245
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-07-07 01:04:23 +0000

    fix: Resolve module exports to be relative

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { Button } from 'Button';
-export { FormField } from 'FormField';
-export { FormSep } from 'FormSep';
-export { IconBtn } from 'IconBtn';
-export { PasswordInput } from 'PasswordInput';
-export { TextInput } from 'TextInput';
+export { Button } from './Button';
+export { FormField } from './FormField';
+export { FormSep } from './FormSep';
+export { IconBtn } from './IconBtn';
+export { PasswordInput } from './PasswordInput';
+export { TextInput } from './TextInput';
