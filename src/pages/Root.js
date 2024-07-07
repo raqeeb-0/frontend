5fdff3cd5454509f93cf054017cc2bfa46d767dc
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-07-07 11:33:40 +0000

    feat: Add landing page body

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,17 +1,31 @@
+import styles from './styles/Root.module.css';
 import { Link } from 'react-router-dom';
 
 
 export const Root = () => {
   return (
     <div>
-      <h1> Hello, From Root! </h1>
-      <br />
-      <h3>
-        Go to ==> &nbsp;
-        <Link to='signup'>
-          Sign up
+      <header className={styles.header}>
+        <Link to='/'>
+          <img
+            src={process.env.PUBLIC_URL + '/logo192name.png'}
+            className={styles.logo}
+            width={128}
+            height={48}
+            alt='Logo'
+          />
         </Link>
-      </h3>
+        <nav>
+          <Link to='/support'> Support </Link>
+          <Link to='/download'> Download </Link>
+          <Link to='/auth/login'> Log in </Link>
+          <Link to='/auth/signup'> Sign up </Link>
+        </nav>
+      </header>
+      <section>
+      </section>
+      <footer>
+      </footer>
     </div>
   );
 }
