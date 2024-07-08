f178874096bf8775a2629d805840eec0fbdbbde5
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-07-08 11:54:06 +0000

    feat: Add landing page header

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,28 +1,48 @@
 import styles from './styles/Root.module.css';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { isLoggedIn } from '../api/auth';
 
 
 export const Root = () => {
+  const [authStatus, setAuthStatus] = useState('');
+
+  useEffect(() => {
+    isLoggedIn()
+      .then((r) => setAuthStatus(r.status))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div>
       <header className={styles.header}>
-        <Link to='/'>
-          <img
-            src={process.env.PUBLIC_URL + '/logo192name.png'}
-            className={styles.logo}
-            width={128}
-            height={48}
-            alt='Logo'
-          />
-        </Link>
-        <nav>
-          <Link to='/support'> Support </Link>
-          <Link to='/download'> Download </Link>
-          <Link to='/auth/login'> Log in </Link>
-          <Link to='/auth/signup'> Sign up </Link>
-        </nav>
+        <section className={styles.headerSection}>
+          <Link to='/'>
+            <img
+              src={process.env.PUBLIC_URL + '/logo192name.png'}
+              className={styles.logo}
+              width={128}
+              height={64}
+              alt='Logo'
+            />
+          </Link>
+          <nav className={styles.nav}>
+            <Link to='/support' className={styles.navLink}> Support </Link>
+            <Link to='/download' className={styles.navLink}> Download </Link>
+            <div className={styles.separator}></div>
+            <Link to='/auth/login' className={styles.navLink}> Log in </Link>
+            <Link
+              to='/auth/signup'
+              className={`${styles.navLink} ${styles.CTA}`}
+            > Get Started </Link>
+          </nav>
+        </section>
       </header>
       <section>
+        <div>
+          Test Is User Logged in: <br />
+          { authStatus } 
+        </div>
       </section>
       <footer>
       </footer>
